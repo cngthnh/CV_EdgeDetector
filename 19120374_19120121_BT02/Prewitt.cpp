05dbac6cf9552b3f88e5c8de7f813051c859639d
nguyenlequang <nepin1042001@gmail.com>
2022-03-28 15:01:36 +0700

    update

--- 19120374_19120121_BT02/Prewitt.cpp
+++ 19120374_19120121_BT02/Prewitt.cpp
@@ -3,14 +3,15 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-vector<vector<char>> filter3x3_y = { {-1,0,1},{-1,0,1},{-1,0,1} };
+// cac mask filter theo ti le 3x3 va 5x5
+vector<vector<char>> filter3x3_y = { {-1,0,1},{-1,0,1},{-1,0,1} }; 
 vector<vector<char>> filter3x3_x = { {-1,-1,-1},{0,0,0},{1,1,1} };
 vector<vector<char>> filter5x5_x = { {9,9,9,9,9}, {9,5,5,5,9},{-7,-3,0,-3,-7},{-7,-3,-3,-3,-7},{-7,-7,-7,-7,-7} };
 vector<vector<char>> filter5x5_y = { {9,9,-7,-7,-7}, {9,5,-3,-3,-7},{9,5,0,-3,-7},{9,5,-3,-3,-7},{9,9,-7,-7,-7} };
 
 int detectByPrewitt(Mat src, Mat& des, int filter, int padding, int stride)
 {
-	switch (filter)
+	switch (filter) // check xem nguoi dung muon  tich ma tran nao
 	{
 	case PREWITT_FILTER::PREWITT_FILTER_3x3:
 		des = convPrewitt(src, filter3x3_x, filter3x3_y, padding, stride);
@@ -25,47 +26,12 @@ int detectByPrewitt(Mat src, Mat& des, int filter, int padding, int stride)
 Mat convPrewitt(Mat& image, vector<vector<char>>& x_mask, vector<vector<char>>& y_mask, int padding, int stride)
 {
 	Mat result;
-	Mat result_x_gradient;
-	Mat result_y_gradient;
-	Mat pad_image;
+	Mat result_x_gradient; // dao ham theo x
+	Mat result_y_gradient; // dao ham theo y
 
-	if (image.type() != CV_8UC1)
-	{
-		cout << "Invalid input image\n";
-		return result;
-	}
-
-	// "valid" padding
-	if (padding == 0)
-	{
-		pad_image = image;
-	}
-	// "same" padding
-	else if (padding == -1)
-	{
-		int padding_horiz = ceil((stride * (image.cols - 1) - image.cols + x_mask[0].size()) / 2);
-		int padding_vert = ceil((stride * (image.rows - 1) - image.rows + x_mask.size()) / 2);
-		copyMakeBorder(image, pad_image, padding_vert, padding_vert, padding_horiz, padding_horiz, BORDER_CONSTANT, (0, 0, 0));
-	}
-	else
-	{
-		copyMakeBorder(image, pad_image, padding, padding, padding, padding, BORDER_CONSTANT, (0, 0, 0));
-	}
-
-	result = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-	result_x_gradient = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-	result_y_gradient = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-
-	for (int y = 0; y < ceil((pad_image.rows - x_mask.size()) / stride); ++y)
-	{
-		for (int x = 0; x < ceil(pad_image.cols - x_mask[0].size() / stride); ++x)
-		{
-			result_x_gradient.at<uchar>(y, x) = saturate_cast<uchar>(applyKernel(pad_image, x_mask, x * stride, y * stride));
-			result_y_gradient.at<uchar>(y, x) = saturate_cast<uchar>(applyKernel(pad_image, y_mask, x * stride, y * stride));
-			result.at<uchar>(y, x) = saturate_cast<uchar>(abs(result_x_gradient.at<uchar>(y, x)) + abs(result_y_gradient.at<uchar>(y, x)));
-
-		}
-	}
+	result_x_gradient = conv(image, x_mask, padding, stride);
+	result_y_gradient = conv(image, y_mask, padding, stride);
+	result = result_x_gradient + result_y_gradient;  // tong 2 dao ham lai
 	imshow("x_gradient", result_x_gradient);
 	imshow("y_gradient", result_y_gradient);
 	return result;
--- 19120374_19120121_BT02/sobel.cpp
+++ 19120374_19120121_BT02/sobel.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+// ma tran tich chap 3x3 hoac 5x5
 vector<vector<char>> y_filter3x3 = { {-1,0,1},{-2,0,2},{-1,0,1} };
 vector<vector<char>> x_filter3x3 = { {1,2,1},{0,0,0},{-1,-2,-1} };
 vector<vector<char>> y_filter5x5 = { {2,2,4,2,2}, {1,1,2,1,1},{0,0,0,0,0},{-1,-1,-2,-1,-1},{-2,-2,-4,-2,-2} };
@@ -9,7 +10,7 @@ vector<vector<char>> x_filter5x5 = { {2,1,0,-1,-2}, {2,1,0,-1,-2},{4,2,0,-2,-4},
 
 int detectBySobel(Mat src, Mat& des, int filter, int padding, int stride)
 {
-	switch (filter)
+	switch (filter)  // cho nguoi dung chon tich chap
 	{
 	case SOBEL_FILTER::SOBEL_FILTER_3x3:
 		des = convSobel(src, x_filter3x3, y_filter3x3, padding, stride);
@@ -26,46 +27,11 @@ Mat convSobel(Mat& image, vector<vector<char>>&x_mask, vector<vector<char>>& y_m
 	Mat result;
 	Mat result_x_gradient;
 	Mat result_y_gradient;
-	Mat pad_image;
-
-	if (image.type() != CV_8UC1)
-	{
-		cout << "Invalid input image\n";
-		return result;
-	}
-
-	// "valid" padding
-	if (padding == 0)
-	{
-		pad_image = image;
-	}
-	// "same" padding
-	else if (padding == -1)
-	{
-		int padding_horiz = ceil((stride * (image.cols - 1) - image.cols + x_mask[0].size()) / 2);
-		int padding_vert = ceil((stride * (image.rows - 1) - image.rows + x_mask.size()) / 2);
-		copyMakeBorder(image, pad_image, padding_vert, padding_vert, padding_horiz, padding_horiz, BORDER_CONSTANT, (0, 0, 0));
-	}
-	else
-	{
-		copyMakeBorder(image, pad_image, padding, padding, padding, padding, BORDER_CONSTANT, (0, 0, 0));
-	}
-
-	result = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-	result_x_gradient = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-	result_y_gradient = Mat::zeros((pad_image.rows - x_mask.size()) / stride + 1, (pad_image.rows - x_mask[0].size()) / stride + 1, CV_8UC1);
-
-	for (int y = 0; y < ceil((pad_image.rows - x_mask.size()) / stride); ++y)
-	{
-		for (int x = 0; x < ceil(pad_image.cols - x_mask[0].size() / stride); ++x)
-		{
-			result_x_gradient.at<uchar>(y, x) = saturate_cast<uchar>(applyKernel(pad_image, x_mask, x * stride, y * stride));
-			result_y_gradient.at<uchar>(y, x) = saturate_cast<uchar>(applyKernel(pad_image, y_mask, x * stride, y * stride));
-			result.at<uchar>(y, x) = saturate_cast<uchar>(abs(result_x_gradient.at<uchar>(y, x)) + abs(result_y_gradient.at<uchar>(y, x)));
-
-		}
-	}
+	
+	result_x_gradient = conv(image, x_mask, padding, stride);
+	result_y_gradient = conv(image, y_mask, padding, stride);
+	result = result_x_gradient + result_y_gradient;
 	imshow("x_gradient", result_x_gradient);
-	imshow("y_gradient", result_y_gradient);
+	imshow("y_gradient", result_y_gradient);	
 	return result;
 }
